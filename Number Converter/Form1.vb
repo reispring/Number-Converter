a0ee9be9e06dfefb60affec5508e9e9b7c7e70e9
reispring <andreimbadere@gmail.com>
2025-09-16 19:36:58 +0800

    i canged the code from convertint32 to parse

--- Number Converter/Form1.vb
+++ Number Converter/Form1.vb
@@ -1,10 +1,21 @@
 ﻿Public Class Form1
+    Private numberWords() As String = {"zero", "one", "two", "three", "four",
+                                       "five", "six", "seven", "eight", "nine", "ten"}
+
+    Private Function WordParse(input As String) As Integer
+        Dim index As Integer = Array.IndexOf(numberWords, input.ToLower().Trim())
+        If index = -1 Then
+            Throw New FormatException("Invalid number word")
+        End If
+        Return index
+    End Function
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Try
             Dim input As String = TextBox1.Text
-            Dim number As Integer = Convert.ToInt32(input)
+            Dim number As Integer = WordParse(input)
 
-            Label1.Text = "You entered the number: " & number.ToString()
+            Label1.Text = "The result is " & number.ToString()
             Label1.ForeColor = Color.Green
 
         Catch ex As FormatException
